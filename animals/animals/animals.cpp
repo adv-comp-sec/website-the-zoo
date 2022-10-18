1f8adc6d1d2b8151f4d48c59e660b8a32e249319
HYEWONLEE\hyewon <katelavande@gmail.com>
2022-10-17 21:41:05 -0400

    change the structure

--- animals/animals/animals.cpp
+++ animals/animals/animals.cpp
@@ -2,20 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #pragma warning(disable : 4996)
+#pragma warning(disable : 6001)
 
 /* ---------------------------------------------------------------------------------------------------------
 	Since we've passed the parameters into this CGI application using the GET method - we can access the
 	parameters and values passed in using the QUERY_STRING environment variable - remembering
 	that we need to parse the parameters and their values out as "key-value" pairs
    --------------------------------------------------------------------------------------------------------- */
-#define kNameLength 20
+#define kNameLength	50
 #define kAnimalLength 5
+#define kAddressLength 50
+#define SUCCESS 0
 
 int main(void)
 {
-	char* data;						// variable used to grab/point to the environment variable(s)
-	char name[kNameLength];			// variable to store the user name
-	char animal[kAnimalLength];		// animal choose by user
+	char* data;							// variable used to grab/point to the environment variable(s)
+	char name[kNameLength];				// user name
+	char animal[kAnimalLength];			// animal code choose by user
+	char animalName[kAnimalLength];					// animal name
+	char descriptionAddress[kAddressLength];
 
 	printf("Content-type:text/html\r\n\r\n"); // output MIME header
 
@@ -23,55 +28,100 @@ int main(void)
 	printf("<head>");
 	printf("<title>The Movies Zoo</title>");
 	printf("</head>\n");
+
+	printf("<style>");
+	printf("body {background-color: #F0F8FF;}\n");
+	printf(".content {display: flex; flex-direction: row; justify-content: center; align-items: center}\n");
+	printf("#description {padding: 40px; align-items: center;}");
+	printf("</style>");
+
 	printf("<body>\n");
-	printf("<div name = ''description' id = 'description'>");
-	printf("<h1>The Movies Zoo</h1><p>Hello!</br>In our Zoo, you can check on some animals that got popular because of movies, series, and cartoons.</br>Find out their scientific names, where they live, what they eat, and what premiere made them superstars!</p></div>");
 
 	// get the information passed into the CGI program and scanf it out ...
 	data = getenv("QUERY_STRING");
-	if (sscanf(data, "name=%s", name) == 1)
+	if (sscanf(data, "name=%[^&]&animal=%s",&name, &animal) != 2)
 	{
-		if (name[0] != '+')
-		{
-			printf("Welcome to our virtual Zoo, %s! </br> Choose one of the animals in the list to know more about it.", name);
-			printf("<form name='animalForm' action = 'animals.exe' method = 'GET'><fieldset>");
-			printf("<div name ='animalsList' id ='animalsList'>");
-			printf("<label for = 'animal'>Animals:");
-			printf("<select name = 'animal' id = 'animal'>");
-			printf("<option value = 'none'>");
-			printf("<option value = 'wolf'>Wolf");
-			printf("<option value = 'orca'>Orca");
-			printf("<option value = 'sqrl'>Squirrel");
-			printf("<option value = 'elph'>Elephant");
-			printf("<option value = 'grff'>Giraffe");
-			printf("<option value = 'lion'>Lion");
-			printf("<input type = 'submit' value = 'Check on animal' > ");
+		printf("Invalid Input");
+		return -1;
+	}
 
-			data = getenv("QUERY_STRING");
-			if (sscanf(data, "animal=%s", animal) == 1 && strcmp(animal, "none") != 0)
-			{
-				printf("It worked");
-			}
-			else
-			{
-				printf("<div id = 'errorAnimal' style = 'color:red;'></div>");
-				
-			}
-			printf("</div>");
-			
-		}
-		else
-		{
-			printf("<div id = 'errorName' style = 'color:red; '></div>");
-		}
+	char* changeBlank = strchr(name, '+');
+	*changeBlank = ' ';
+
+	if (strcmp(animal, "wolf") == SUCCESS)
+	{
+		strcpy(animalName, "Wolf");
+		strcpy(descriptionAddress, "./theZoo/descript_wolf.txt");
+	}
+	else if (strcmp(animal, "orca") == SUCCESS)
+	{
+		strcpy(animalName, "Orca");
+		strcpy(descriptionAddress, "./theZoo/descript_orca.txt");
+	}
+	else if (strcmp(animal, "sqrl") == SUCCESS)
+	{
+		strcpy(animalName, "Squirrel");
+		strcpy(descriptionAddress, "./theZoo/descript_sqrl.txt");
+	}
+	else if (strcmp(animal, "elph") == SUCCESS)
+	{
+		strcpy(animalName, "Elephant");
+		strcpy(descriptionAddress, "./theZoo/descript_elph.txt");
+	}
+	else if (strcmp(animal, "grff") == SUCCESS)
+	{
+		strcpy(animalName, "Giraffe");
+		strcpy(descriptionAddress, "./theZoo/descript_grff.txt");
+	}
+	else if (strcmp(animal, "lion") == SUCCESS)
+	{
+		strcpy(animalName, "Lion");
+		strcpy(descriptionAddress, "./theZoo/descript_lion.txt");
 	}
 	else
 	{
-		printf("<div id = 'errorName' style = 'color:red; '></div>");
+		printf("Invalid input");
+		return -1;
 	}
 
+	printf("<div class = \"content\">");
+	printf("<div id = \"image\">");
+	printf("<img src='./theZoo/image_%s.jpg' alt='%s' width=400 height=400><br>", animal, animalName);
+	printf("</div>");
+
+	printf("<div id = \"description\">");
+	printf("<h1> Hello, %s <br> <h1>", name);
+	printf("<h1> You choose %s </h1>", animalName);
+
+	FILE* myfile = fopen(descriptionAddress, "r");
+	if (myfile == NULL)
+	{
+		printf("Unable to open file!");
+		return -1;
+	}
+
+	char buffer[255];
+
+	while (!feof(myfile)) {
+		if (fgets(buffer, sizeof buffer, myfile) != 0 && *buffer != '\n')
+		{
+			int len = strlen(buffer);
+			if (len > 0 && buffer[len - 1] == '\n')
+			{
+				buffer[len - 1] = 0;
+			}
+			printf("%s", buffer);
+
+		}
+	}
+
+	fclose(myfile);
+
+	printf("</div>");
+	printf("</div>");
+
 	printf("</body></html>");
-	
+
 	return 0;
 }
 
